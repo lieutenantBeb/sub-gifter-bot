6e8d18cd47fbaaf812f602e1354707356face56b
Ben Brown <benjamin.brown061@gmail.com>
2022-07-01 23:42:06 -0400

    starting over

--- commands/gn.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'gn',
-    aliases: [],
-    description: "Tell everyone gn ",
-    async execute(client, message, args, Discord){
-        message.channel.send('goodnight+ggs+sleep well+ratio+its fuckin bedtime+dont let the bed bugs bite+sweet dreams+sleep tight+bye+see ya tomorrow');
-    }
-}
--- commands/smack.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'smack',
-    aliases: [],
-    description: "Smack a biatch",
-    async execute(client, message, args, Discord){
-        message.channel.send('<a:smack:980673964772835329>');
-    }
-}
