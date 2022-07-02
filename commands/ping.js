28232b0edf8fa7483c85524ef2be68ded4fef9be
Ben Brown <benjamin.brown061@gmail.com>
2022-07-01 23:50:12 -0400

    starting over

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    aliases: [],
-    description: "this is a ping command",
-    async execute(client, message, args, Discord){
-        message.channel.send('pong');
-    }
-}
