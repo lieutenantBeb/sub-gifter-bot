68a1ed0cb3236f48b564b02afc9b3e81c427affb
Benjamin Brown <benjamin.brown061@gmail.com>
2022-07-06 18:47:14 -0400

    ratio

---
+++ commands/gm.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'gm',
+    aliases: [],
+    description: "Tell everyone gm ",
+    async execute(client, message, args, Discord){
+        message.channel.send('good morning+rise and shine+did you sleep well?+ratio+wake up+get some coffee damn+hope you have a good day+see ya later');
+    }
+}
